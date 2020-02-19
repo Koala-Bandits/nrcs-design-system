0b92b24af6535f3b6166baf78be6233ca3a23e18
Ian Erickson <ian.erickson@usda.gov>
2020-02-19 12:03:04 -0700

    Added outline stories for Tables and DataTable.

---
+++ src/components/tables/DataTable.stories.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+// import { Alert, Container, Row, Col } from "reactstrap";
+// import Icon from "@mdi/react";
+// import {
+//   mdiInformation,
+//   mdiAlert,
+//   mdiAlertCircle,
+//   mdiCheckCircle
+// } from "@mdi/js";
+
+export default {
+  title: "Components|/Data Table"
+};
+
+export const DataTableDefault = () => (
+  <div>
+    <h1>Data Table - Default</h1>
+    <p>
+      To Do... FPAC style table with gray header, no zebra, horiz. lines, light
+      gray hover, light blue selection w/ bold styles...
+    </p>
+    <p>
+      ... WITH row selection, indicatior column, status column with
+      badge-labels, actions menu, and upper responsive layout for table ations
+      and paging controls all integrated as a suggested implementation.
+    </p>
+  </div>
+);
+
+DataTableDefault.story = {
+  name: "Data Table - Default"
+};
---
+++ src/components/tables/Table.stories.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+// import { Alert, Container, Row, Col } from "reactstrap";
+// import Icon from "@mdi/react";
+// import {
+//   mdiInformation,
+//   mdiAlert,
+//   mdiAlertCircle,
+//   mdiCheckCircle
+// } from "@mdi/js";
+
+export default {
+  title: "Components|/Tables"
+};
+
+export const TablesDefault = () => (
+  <div>
+    <h1>Tables - Default</h1>
+    <p>
+      To Do... FPAC style table with gray header, no zebra, horiz. lines, light
+      gray hover, light blue selection w/ bold styles.
+    </p>
+  </div>
+);
+
+export const TablesAlt = () => (
+  <div>
+    <h1>Table - Alternate</h1>
+
+    <p>
+      To Do... FPAC style table with no header and/or no boxed border for use in
+      showing metadata or uses in FundsManager and smaller displays.
+    </p>
+  </div>
+);
+
+TablesDefault.story = {
+  name: "Tables - Default"
+};
+
+TablesAlt.story = {
+  name: "Tables - Alternate"
+};
