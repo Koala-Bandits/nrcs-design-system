0623b061f3cd939b55c1f16dffab1836633a2db3
Ian Erickson <eddieshowcase@gmail.com>
2020-02-28 10:26:51 -0700

    Added split dropdowns.

--- src/components/dropdowns/Dropdown.stories.js
+++ src/components/dropdowns/Dropdown.stories.js
@@ -3,6 +3,8 @@ import {
   Container,
   Row,
   Col,
+  Button,
+  ButtonDropdown,
   Dropdown,
   DropdownToggle,
   DropdownMenu,
@@ -17,10 +19,20 @@ export default {
 
 export const DropdownDefault = () => {
   const [primaryDropdownOpen, setPrimaryDropdownOpen] = useState(false);
+  const [primarySplitDropdownOpen, setPrimarySplitDropdownOpen] = useState(
+    false
+  );
   const [secondaryDropdownOpen, setSecondaryDropdownOpen] = useState(false);
+  const [secondarySplitDropdownOpen, setSecondarySplitDropdownOpen] = useState(
+    false
+  );
   const [flatDropdownOpen, setFlatDropdownOpen] = useState(false);
 
   const togglePrimary = () => setPrimaryDropdownOpen(prevState => !prevState);
+  const togglePrimarySplit = () =>
+    setPrimarySplitDropdownOpen(prevState => !prevState);
+  const toggleSecondarySplit = () =>
+    setSecondarySplitDropdownOpen(prevState => !prevState);
   const toggleSecondary = () =>
     setSecondaryDropdownOpen(prevState => !prevState);
   const toggleFlat = () => setFlatDropdownOpen(prevState => !prevState);
@@ -29,60 +41,97 @@ export const DropdownDefault = () => {
     <div>
       <h1 className="docs">Dropdown - Default</h1>
 
-      <Container>
-        <Row>
-          <Col>
-            <Dropdown isOpen={primaryDropdownOpen} toggle={togglePrimary}>
-              <DropdownToggle color="primary">
-                Dropdown
-                <Icon className="mdi ml-1" path={mdiChevronDown} />
-              </DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem header>Header</DropdownItem>
-                <DropdownItem>Some Action</DropdownItem>
-                <DropdownItem disabled>Action (disabled)</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Foo Action</DropdownItem>
-                <DropdownItem>Bar Action</DropdownItem>
-                <DropdownItem>Quo Action</DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </Col>
-          <Col>
-            <Dropdown isOpen={secondaryDropdownOpen} toggle={toggleSecondary}>
-              <DropdownToggle outline color="primary">
-                Dropdown
-                <Icon className="mdi ml-1" path={mdiChevronDown} />
-              </DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem header>Header</DropdownItem>
-                <DropdownItem>Some Action</DropdownItem>
-                <DropdownItem disabled>Action (disabled)</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Foo Action</DropdownItem>
-                <DropdownItem>Bar Action</DropdownItem>
-                <DropdownItem>Quo Action</DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </Col>
-          <Col>
-            <Dropdown isOpen={flatDropdownOpen} toggle={toggleFlat}>
-              <DropdownToggle color="link">
-                <Icon className="mdi ml-1" path={mdiDotsVertical} />
-              </DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem header>Header</DropdownItem>
-                <DropdownItem>Some Action</DropdownItem>
-                <DropdownItem disabled>Action (disabled)</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Foo Action</DropdownItem>
-                <DropdownItem>Bar Action</DropdownItem>
-                <DropdownItem>Quo Action</DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </Col>
-        </Row>
-      </Container>
+      <Row xs="1" sm="3" md="5">
+        <Col className="mb-3">
+          <Dropdown isOpen={primaryDropdownOpen} toggle={togglePrimary}>
+            <DropdownToggle color="primary">
+              Dropdown
+              <Icon className="mdi ml-1" path={mdiChevronDown} />
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Header</DropdownItem>
+              <DropdownItem>Some Action</DropdownItem>
+              <DropdownItem disabled>Action (disabled)</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Foo Action</DropdownItem>
+              <DropdownItem>Bar Action</DropdownItem>
+              <DropdownItem>Quo Action</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </Col>
+        <Col className="mb-3">
+          <ButtonDropdown
+            isOpen={primarySplitDropdownOpen}
+            toggle={togglePrimarySplit}
+            className="dropdown"
+          >
+            <Button color="primary">Primary Split</Button>
+            <DropdownToggle color="primary" className="rounded-right">
+              <Icon className="mdi" path={mdiChevronDown} />
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Header</DropdownItem>
+              <DropdownItem disabled>Action</DropdownItem>
+              <DropdownItem>Another Action</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Another Action</DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>
+        </Col>
+        <Col className="mb-3">
+          <Dropdown isOpen={secondaryDropdownOpen} toggle={toggleSecondary}>
+            <DropdownToggle outline color="primary">
+              Dropdown
+              <Icon className="mdi ml-1" path={mdiChevronDown} />
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Header</DropdownItem>
+              <DropdownItem>Some Action</DropdownItem>
+              <DropdownItem disabled>Action (disabled)</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Foo Action</DropdownItem>
+              <DropdownItem>Bar Action</DropdownItem>
+              <DropdownItem>Quo Action</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </Col>
+        <Col className="mb-3">
+          <ButtonDropdown
+            isOpen={secondarySplitDropdownOpen}
+            toggle={toggleSecondarySplit}
+          >
+            <Button outline color="primary">
+              Seondary Split
+            </Button>
+            <DropdownToggle outline color="primary" className="rounded-right">
+              <Icon className="mdi" path={mdiChevronDown} />
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Header</DropdownItem>
+              <DropdownItem disabled>Action</DropdownItem>
+              <DropdownItem>Another Action</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Another Action</DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>
+        </Col>
+        <Col className="mb-3">
+          <Dropdown isOpen={flatDropdownOpen} toggle={toggleFlat}>
+            <DropdownToggle color="link">
+              <Icon className="mdi ml-1" path={mdiDotsVertical} />
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Header</DropdownItem>
+              <DropdownItem>Some Action</DropdownItem>
+              <DropdownItem disabled>Action (disabled)</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Foo Action</DropdownItem>
+              <DropdownItem>Bar Action</DropdownItem>
+              <DropdownItem>Quo Action</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </Col>
+      </Row>
     </div>
   );
 };
