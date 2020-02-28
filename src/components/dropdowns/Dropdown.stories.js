85ece52584f31a6f71c62ecd6ea2421bf2b86919
Ian Erickson <eddieshowcase@gmail.com>
2020-02-28 09:59:17 -0700

    Dropdowns done.

--- src/components/dropdowns/Dropdown.stories.js
+++ src/components/dropdowns/Dropdown.stories.js
@@ -1,96 +1,88 @@
 import React, { useState } from "react";
 import {
+  Container,
+  Row,
+  Col,
   Dropdown,
   DropdownToggle,
   DropdownMenu,
   DropdownItem
 } from "reactstrap";
 import Icon from "@mdi/react";
-import { mdiDotsVertical } from "@mdi/js";
+import { mdiChevronDown, mdiDotsVertical } from "@mdi/js";
 
 export default {
   title: "Components|Dropdowns"
 };
 
 export const DropdownDefault = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [primaryDropdownOpen, setPrimaryDropdownOpen] = useState(false);
+  const [secondaryDropdownOpen, setSecondaryDropdownOpen] = useState(false);
+  const [flatDropdownOpen, setFlatDropdownOpen] = useState(false);
 
-  const toggle = () => setDropdownOpen(prevState => !prevState);
+  const togglePrimary = () => setPrimaryDropdownOpen(prevState => !prevState);
+  const toggleSecondary = () =>
+    setSecondaryDropdownOpen(prevState => !prevState);
+  const toggleFlat = () => setFlatDropdownOpen(prevState => !prevState);
 
   return (
     <div>
       <h1 className="docs">Dropdown - Default</h1>
 
-      <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-        <DropdownToggle caret>Dropdown</DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem header>Header</DropdownItem>
-          <DropdownItem>Some Action</DropdownItem>
-          <DropdownItem disabled>Action (disabled)</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>Foo Action</DropdownItem>
-          <DropdownItem>Bar Action</DropdownItem>
-          <DropdownItem>Quo Action</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-
-      <h2>Action Menu Icon</h2>
-
-      <DropdownToggle>
-        <Icon className="mdi" path={mdiDotsVertical} />
-      </DropdownToggle>
-      <DropdownMenu>
-        <DropdownItem tag="a" href="/blah" active>
-          Link
-        </DropdownItem>
-      </DropdownMenu>
-
-      <div class="dropdown">
-        <button
-          class="btn btn-secondary dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >
-          Dropdown button
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a class="dropdown-item" href="#">
-            Action
-          </a>
-          <a class="dropdown-item" href="#">
-            Another action
-          </a>
-          <a class="dropdown-item" href="#">
-            Something else here
-          </a>
-        </div>
-      </div>
-
-      <div class="dropdown">
-        <Icon
-          className="mdi dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-          path={mdiDotsVertical}
-        />
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a class="dropdown-item" href="#">
-            Action
-          </a>
-          <a class="dropdown-item" href="#">
-            Another action
-          </a>
-          <a class="dropdown-item" href="#">
-            Something else here
-          </a>
-        </div>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <Dropdown isOpen={primaryDropdownOpen} toggle={togglePrimary}>
+              <DropdownToggle color="primary">
+                Dropdown
+                <Icon className="mdi ml-1" path={mdiChevronDown} />
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem header>Header</DropdownItem>
+                <DropdownItem>Some Action</DropdownItem>
+                <DropdownItem disabled>Action (disabled)</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Foo Action</DropdownItem>
+                <DropdownItem>Bar Action</DropdownItem>
+                <DropdownItem>Quo Action</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </Col>
+          <Col>
+            <Dropdown isOpen={secondaryDropdownOpen} toggle={toggleSecondary}>
+              <DropdownToggle outline color="primary">
+                Dropdown
+                <Icon className="mdi ml-1" path={mdiChevronDown} />
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem header>Header</DropdownItem>
+                <DropdownItem>Some Action</DropdownItem>
+                <DropdownItem disabled>Action (disabled)</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Foo Action</DropdownItem>
+                <DropdownItem>Bar Action</DropdownItem>
+                <DropdownItem>Quo Action</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </Col>
+          <Col>
+            <Dropdown isOpen={flatDropdownOpen} toggle={toggleFlat}>
+              <DropdownToggle color="link">
+                <Icon className="mdi ml-1" path={mdiDotsVertical} />
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem header>Header</DropdownItem>
+                <DropdownItem>Some Action</DropdownItem>
+                <DropdownItem disabled>Action (disabled)</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Foo Action</DropdownItem>
+                <DropdownItem>Bar Action</DropdownItem>
+                <DropdownItem>Quo Action</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
