7d207f7a8161c15af0ff72ddf7a82fcfdaab1143
Ian Erickson <eddieshowcase@gmail.com>
2020-03-05 21:06:05 -0700

    Header cmpnt styling.

--- src/components/page/header/Header.stories.js
+++ src/components/page/header/Header.stories.js
@@ -6,12 +6,30 @@ export default {
   title: "Page Components|Header"
 };
 
-export const Base = () => (
+export const HeaderNRCS = () => (
   <div className="nrcs-header">
-    <div className="container header-title">
+    <div className="container header-title h1">
       <a href="/home" title="Home" aria-label="Home">
-        NRCS Design System <span className="header-subtitle">| vBETA</span>
+        NRCS Design System <span className="header-subtitle">| BETA</span>
       </a>
     </div>
   </div>
 );
+
+export const HeaderFPAC = () => (
+  <div className="fpac-header">
+    <div className="container header-title h1">
+      <a href="/home" title="Home" aria-label="Home">
+        NRCS Design System <span className="header-subtitle">| FPAC green</span>
+      </a>
+    </div>
+  </div>
+);
+
+HeaderNRCS.story = {
+  name: "Header - NRCS"
+};
+
+HeaderFPAC.story = {
+  name: "Header - FPAC"
+};
