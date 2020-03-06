f5b86e8184ee4b5dc4adb791e8131b4ff66e9209
Ian Erickson <eddieshowcase@gmail.com>
2020-03-06 11:23:15 -0700

    Improve header stories.

--- src/components/page/header/Header.stories.js
+++ src/components/page/header/Header.stories.js
@@ -8,11 +8,38 @@ export default {
   title: "Page Components|Header"
 };
 
-export const StoryHeaderNRCS = () => <HeaderNrcs />;
+export const StoryHeaderNRCS = () => {
+  return (
+    <div>
+      <h1 className="docs">Header - NRCS</h1>
+      <p>NRCS Header is uses primary blue and is responsive in sizing.</p>
+      <HeaderNrcs />
+    </div>
+  );
+};
 
-export const StoryHeaderFPAC = () => <HeaderFpac />;
+export const StoryHeaderFPAC = () => {
+  return (
+    <div>
+      <h1 className="docs">Header - FPAC</h1>
+      <p>FPAC Header is uses secondary green and is responsive in sizing.</p>
+      <HeaderFpac />
+    </div>
+  );
+};
 
-export const StoryHeaderCD = () => <HeaderCD />;
+export const StoryHeaderCD = () => {
+  return (
+    <div>
+      <h1 className="docs">Header - NRCS</h1>
+      <p>
+        CD Header is uses primary blue, smaller, and vertically centered items.
+        It is also responsive in sizing.
+      </p>
+      <HeaderCD />
+    </div>
+  );
+};
 
 StoryHeaderNRCS.story = {
   name: "Header - NRCS"
