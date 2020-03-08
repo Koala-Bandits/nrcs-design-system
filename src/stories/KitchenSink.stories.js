6b8d1e70a1d1d259dddb2cd5e18148fb9063ac71
Ian Erickson <eddieshowcase@gmail.com>
2020-03-07 20:47:15 -0700

    Kitchen sink es functional.

--- src/stories/KitchenSink.stories.js
+++ src/stories/KitchenSink.stories.js
@@ -1,15 +1,33 @@
 import React from "react";
+import { getStorybook } from "@storybook/react";
 
 export default {
   title: "Design System|Kitchen Sink"
 };
+
+const allStories = getStorybook();
 
 export const KitchenSinkStory = () => (
   <div className="container">
     <h1 className="docs">Kitchen Sink</h1>
 
     <p>
-      <span className="text-danger">** Under Construction **</span>
+      <span className="text-danger">
+        This following is every story in our Storybook rendered in succession.
+      </span>
     </p>
+
+    <>
+      {allStories.map(stories => {
+        return (
+          <>
+            {" "}
+            {stories.stories.map(story => {
+              return <>{story.render()}</>;
+            })}{" "}
+          </>
+        );
+      })}
+    </>
   </div>
 );
