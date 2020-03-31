ee822628ff99ab1cb4cb433c9a922b7a9f9c5acb
Ian Erickson <eddieshowcase@gmail.com>
2020-03-31 13:30:39 -0600

    Nowrap and horizontal scrolling overflow for tabs on small devices like FPAC.

--- src/components/listgroups/ListGroup.stories.js
+++ src/components/listgroups/ListGroup.stories.js
@@ -71,11 +71,11 @@ export const ListGroupStory = () => (
         This items is <strong>active/selected</strong>
       </ListGroupItem>
       <ListGroupItem>
-        <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">List item heading</h5>
+        <div className="d-flex w-100 justify-content-between">
+          <h5 className="mb-1">List item heading</h5>
           <small>3 days ago</small>
         </div>
-        <p class="mb-1">
+        <p className="mb-1">
           Donec id elit non mi porta gravida at eget metus. Maecenas sed diam
           eget risus varius blandit.
         </p>
