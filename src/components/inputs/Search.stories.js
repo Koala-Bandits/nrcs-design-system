d0b060f1eb0cfd3e4b61e971b76a5e6488f19205
Ian Erickson <eddieshowcase@gmail.com>
2020-05-07 14:33:06 -0600

    Start to a search component.

--- src/components/inputs/Search.stories.js
+++ src/components/inputs/Search.stories.js
@@ -1,49 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButtonDropdown,
+  Input,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Row,
+  Col
+} from "reactstrap";
+
+import { PrimaryButton, FlatButton } from "components/buttons/Buttons";
+
+import Icon from "@mdi/react";
+import { mdiMagnify } from "@mdi/js";
+
+//component
+import Search from "components/inputs/Search";
 
 export default {
   title: "Components|Inputs/Search"
 };
 
-export const FormDefault = () => (
-  <div>
-    <h2 className="docs">Form - Default</h2>
-    To do... example vertical layout form with top-aligned labels.
-  </div>
-);
+export const SearchDefault = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [splitButtonOpen, setSplitButtonOpen] = useState(false);
 
-export const FormHorizontal = () => (
-  <div>
-    <h2 className="docs">Form - Horizontal</h2>
-    To do... example horizontal layout form with left-aligned labels.
-  </div>
-);
+  const toggleDropDown = () => setDropdownOpen(!dropdownOpen);
 
-export const FormInline = () => (
-  <div>
-    <h2 className="docs">Form - Inline</h2>
-    To do... example horizontal layout form with left-aligned labels.
-  </div>
-);
+  return (
+    <div>
+      <h2 className="docs">Search - Default</h2>
+      Basic search field.
+      <Row>
+        <Col sm="12" md="6">
+          <p>This is my component:</p>
+          <Search />
 
-export const FormInputGroup = () => (
-  <div>
-    <h2 className="docs">Form - InputGroup</h2>
-    To do... various input grouping examples.
-  </div>
-);
+          <p>TEMPORARY:</p>
 
-FormDefault.story = {
-  name: "Form - Default"
+          <InputGroup>
+            <Input />
+            <InputGroupAddon addonType="append">
+              <PrimaryButton>Search</PrimaryButton>
+            </InputGroupAddon>
+          </InputGroup>
+          <p>Icon only</p>
+
+          <InputGroup>
+            <Input />
+            <InputGroupAddon addonType="append">
+              <PrimaryButton className="btn-icon">
+                <Icon className="mdi" path={mdiMagnify} />
+              </PrimaryButton>
+            </InputGroupAddon>
+          </InputGroup>
+
+          <h3>Small</h3>
+
+          <InputGroup>
+            <Input size="sm" />
+            <InputGroupAddon addonType="append">
+              <PrimaryButton size="sm">Search</PrimaryButton>
+            </InputGroupAddon>
+          </InputGroup>
+          <p>Icon only</p>
+          <InputGroup>
+            <Input size="sm" />
+            <InputGroupAddon addonType="append">
+              <PrimaryButton size="sm" className="btn-icon">
+                <Icon className="mdi" path={mdiMagnify} size={0.8} />
+              </PrimaryButton>
+            </InputGroupAddon>
+          </InputGroup>
+
+          <h3>Filter</h3>
+          <InputGroup>
+            <Input />
+            <InputGroupButtonDropdown
+              addonType="append"
+              isOpen={dropdownOpen}
+              toggle={toggleDropDown}
+            >
+              <DropdownToggle outline caret color="primary">
+                Filter
+              </DropdownToggle>
+
+              <DropdownMenu>
+                <DropdownItem header>Header</DropdownItem>
+                <DropdownItem disabled>Action</DropdownItem>
+                <DropdownItem>Another Action</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Another Action</DropdownItem>
+              </DropdownMenu>
+            </InputGroupButtonDropdown>
+            <InputGroupAddon addonType="append">
+              <PrimaryButton>Search</PrimaryButton>
+            </InputGroupAddon>
+          </InputGroup>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
-FormHorizontal.story = {
-  name: "Form - Horizontal"
-};
-
-FormInline.story = {
-  name: "Form - Inline"
-};
-
-FormInputGroup.story = {
-  name: "Form - Input Group"
+SearchDefault.story = {
+  name: "Search - Default"
 };
