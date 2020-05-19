2a7a971c9ce7c0d2978564a7e306054e46202cb5
Ian Erickson <eddieshowcase@gmail.com>
2020-05-19 14:54:28 -0600

    Rough start to Profiles page...

--- src/wcis/Profiles.js
+++ src/wcis/Profiles.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Card, CardBody, Row, Col } from "reactstrap";
-import { ContentHeaderH1 } from "components/text/ContentHeader";
+import {
+  ContentHeaderH1,
+  ContentHeaderH2
+} from "components/text/ContentHeader";
+import { AlertMessage } from "components/feedback/messages/AlertMessage";
+import Search from "components/inputs/Search";
 
 class Profiles extends React.Component {
   constructor(props) {
@@ -11,11 +16,33 @@ class Profiles extends React.Component {
     return (
       <div className="container">
         <ContentHeaderH1>Profiles</ContentHeaderH1>
-
+        <AlertMessage color="info">
+          Selection a Station and Sensor below to view Profile data.
+        </AlertMessage>
         <Row>
-          <Col>
+          <Col xs="12" md="4" className="mb-3">
+            <h2 className="h3">Stations</h2>
+            <Search />
             <Card className="card-muted">
-              <CardBody className="text-center">CONTENT</CardBody>
+              <CardBody className="text-center">CONTENT A</CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" md="8" className="mb-3">
+            <h2 className="h3">Sensors</h2>
+            <Search />
+            <Card className="card-muted">
+              <CardBody className="text-center">CONTENT B</CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <ContentHeaderH2>[Selection] > Proxy123 Beaverton</ContentHeaderH2>
+        <Row>
+          <Col xs="12" md="4" className="mb-3">
+            graph meta content and options
+          </Col>
+          <Col xs="12" md="8" className="mb-3">
+            <Card className="card-muted">
+              <CardBody className="text-center">GRAPH PLACEHOLDER</CardBody>
             </Card>
           </Col>
         </Row>
