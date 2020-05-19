df9ffe01c493b9e39763cc212e336a33c51809a7
Ian Erickson <eddieshowcase@gmail.com>
2020-05-19 10:54:37 -0600

    Add some padding in layout examples.

--- src/pages/guides/GridAndLayout.js
+++ src/pages/guides/GridAndLayout.js
@@ -81,12 +81,12 @@ class GridAndLayout extends React.Component {
 
         <h3 className="h5 mt-4 mb-3">2-Column Stacking</h3>
         <Row>
-          <Col xs="12" md="8">
+          <Col xs="12" md="8" className="mb-3">
             <Card className="card-muted">
               <CardBody className="text-center">CONTENT A</CardBody>
             </Card>
           </Col>
-          <Col xs="12" md="4">
+          <Col xs="12" md="4" className="mb-3">
             <Card className="card-muted">
               <CardBody className="text-center">CONTENT B</CardBody>
             </Card>
@@ -95,17 +95,17 @@ class GridAndLayout extends React.Component {
 
         <h3 className="h5 mt-4 mb-3">3-Column Stacking</h3>
         <Row>
-          <Col xs="6" sm="4">
+          <Col xs="6" sm="4" className="mb-3">
             <Card className="card-muted">
               <CardBody className="text-center">CONTENT A</CardBody>
             </Card>
           </Col>
-          <Col xs="6" sm="4">
+          <Col xs="6" sm="4" className="mb-3">
             <Card className="card-muted">
               <CardBody className="text-center">CONTENT B</CardBody>
             </Card>
           </Col>
-          <Col xs="12" sm="4">
+          <Col xs="12" sm="4" className="mb-3">
             <Card className="card-muted">
               <CardBody className="text-center">CONTENT C</CardBody>
             </Card>
