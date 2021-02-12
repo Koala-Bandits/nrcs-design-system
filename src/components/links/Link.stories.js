d5a434d054e71030f0c8fb208b580b24dcf137e4
Ian Erickson <eddieshowcase@gmail.com>
2021-02-12 13:55:29 -0700

    Improve link accessiblity and documentation.

--- src/components/links/Link.stories.js
+++ src/components/links/Link.stories.js
@@ -11,14 +11,35 @@ export default {
 export const LinksDefault = () => (
   <div>
     <ContentHeaderH1>Links - Default</ContentHeaderH1>
-    <a href="/#">This is a link</a> - it links somewhere INSIDE our app.
+    <a href="/#">This is a basic link</a> - it links somewhere INSIDE our app.
     <p></p>
-    <a href="/#">
+    <h3 className="mt-3">External</h3>
+    <a
+      href="/#"
+      target="_blank"
+      title="This link will open in a new window or tab"
+      aria-label="This link will open in a new window or tab"
+    >
       This is an external link{" "}
       <Icon className="mdi" path={mdiOpenInNew} size={0.8} />
     </a>{" "}
-    - it links somewhere OUTSIDE of our app. We use the 'open in new' icon to
-    indicate it's external.
+    <p className="mt-3">
+      Typically using <code>target="_blank"</code>, external links go somewhere
+      OUTSIDE of our app.{" "}
+    </p>
+    <h4>Accessibility</h4>
+    <p>
+      For accessibility we use the 'open in new' icon for a visual indication
+      that it will go elsewhere. Also note that the 'title' and/or 'aria-label'
+      are also used to communicate to screen reader users that the link will
+      open a new tab or window.
+    </p>
+    <p>
+      If your use of an anchor tag wraps a button or icon as a link (and
+      therefore the 'open in new' icon is not present), the use of the 'title'
+      attribute causes a browser tooltip to appear which can serve as our visual
+      cue to users that this will open elsewhere.
+    </p>
   </div>
 );
 
