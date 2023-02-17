588ae1b58fac4918131cd7612b59793bc748d3ba
Michael W Lu <michaelwenlu@gmail.com>
2023-02-17 14:12:20 -0500

    Update packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,12 +3,10 @@ const path = require("path");
 // Export a function. Accept the base config as the only param.
 module.exports = {
   // stories: ["../src/stories/0-Welcome.stores.js", "../src/**/*.stories.js"],
-  addons: [
-    "@storybook/preset-create-react-app",
-    // "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-storysource",
-    "@storybook/preset-scss",
-    "@storybook/addon-viewport/register"
-  ]
+  addons: ["@storybook/preset-create-react-app",
+  // "@storybook/addon-actions",
+  "@storybook/addon-links", "@storybook/addon-storysource", "@storybook/preset-scss", "@storybook/addon-viewport/register"],
+  core: {
+    builder: "webpack5"
+  }
 };
